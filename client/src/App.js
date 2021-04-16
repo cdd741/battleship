a2c3ffac2dd23008dc8b0b1ed2d835405aea8ba6
Charles Lu <kuanminglu0529@gmail.com>
2021-04-16 11:51:45 -0700

    attempt at connecting front end and back end, getting 404 error when trying to connect to socket

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,41 @@ import AvailableShips from "./components/AvailableShips/AvailableShips";
 import Board from "./components/Board/Board";
 import Start from "./components/Start/Start";
 import Title from "./components/Title/Title";
+import { board } from "./util/board";
+// import subscribeToGame from "./util/subscribeToGame";
+import socketIOClient from "socket.io-client";
+const ENDPOINT = "http://localhost:8080";
 
 class App extends React.Component {
     constructor(props) {
         super(props);
 
+        this.socket = socketIOClient(ENDPOINT);
+
         this.state = {
             startGame: false,
+            gridArray: board,
+            currentShip: null,
+            hoverPos:{}
         };
     }
+
+    handlePlacementGridClick = (lat, lon) => {
+        console.log(`Clicked on lat:${lat} & lon:${lon}`);
+
+    };
+
+    handleAttackGridClick = (lat, lon) => {
+        console.log(`Clicked on lat:${lat} & lon:${lon}`);
+    };
+
+    handleShipSelect = (ship) => {
+        this.setState({
+            currentShip: ship,
+        });
+        // console.log("currentShip:", this.state.currentShip);
+        // console.log("clicked on:", ship)
+    };
 
     handleClickStart = (event) => {
         event.preventDefault();
@@ -21,17 +47,30 @@ class App extends React.Component {
         });
     };
 
+    componentDidMount = () => {
+      this.socket.on("connection", data=> {
+        console.log(data)
+      })
+    }
+
     render() {
         let game = <Start clickHandler={this.handleClickStart} />;
         if (this.state.startGame) {
             game = (
                 <div className="canvas">
-                  <div className="canvas__boards">
-                    <Board />
-                    <Board />
-                  </div>
-                  <h2>Your available ships</h2>
-                    <AvailableShips />
+                    <div className="canvas__boards">
+                        <Board
+                            gridArray={this.state.gridArray}
+                            handleClick={this.handlePlacementGridClick}
+                            handleMouseEnter = {this.handleMouseEnter}
+                        />
+                        <Board
+                            gridArray={this.state.gridArray}
+                            handleClick={this.handleAttackGridClick}
+                        />
+                    </div>
+                    <h2>Your available ships</h2>
+                    <AvailableShips handleClick={this.handleShipSelect} />
                 </div>
             );
         }
@@ -41,7 +80,13 @@ class App extends React.Component {
                 <header>
                     <Title title="Battleships" />
                 </header>
-                <main>{game}</main>
+                <main
+                    onContextMenu={(event) => {
+                        event.preventDefault();
+                    }}
+                >
+                    {game}
+                </main>
             </div>
         );
     }
--- client/src/components/AvailableShips/AvailableShips.jsx
+++ client/src/components/AvailableShips/AvailableShips.jsx
@@ -6,14 +6,14 @@ import destroyer from '../../assets/images/ShipDestroyerHull.png';
 import carrier from '../../assets/images/ShipCarrierHull.png';
 import battle from '../../assets/images/ShipBattleshipHull.png';
 
-function AvailableShips() {
+function AvailableShips(props) {
     return (
         <div className="available-ships">
-            <img src={submarine} alt="submarine"/>
-            <img src={patrol} alt="patrol ship"/>
-            <img src={destroyer} alt="destroyer"/>
-            <img src={carrier} alt="carrier ship"/>
-            <img src={battle} alt="battleship"/>
+            <img src={submarine} alt="submarine" onClick={() => {props.handleClick(1)}}/>
+            <img src={patrol} alt="patrol ship" onClick={() => {props.handleClick(2)}}/>
+            <img src={destroyer} alt="destroyer" onClick={() => {props.handleClick(3)}}/>
+            <img src={carrier} alt="carrier ship" onClick={() => {props.handleClick(4)}}/>
+            <img src={battle} alt="battleship" onClick={() => {props.handleClick(5)}}/>
         </div>
     )
 }
