2294939585192b538894586aaa4cd6b66dabecc2
Baizhou Chen <b99chen@edu.uwaterloo.ca>
2021-04-16 14:54:18 -0400

    under developing backend socketio api
    Signed-off-by: Baizhou Chen <b99chen@edu.uwaterloo.ca>

--- server/server.js
+++ server/server.js
@@ -1,15 +1,26 @@
 const express = require("express");
+const app = express();
 const cors = require("cors");
 const port = 8080;
 const http = require("http");
+const socket = require("./socket");
+// app.use(cors());
+
 const server = http.createServer(app);
 const io = require("socket.io")(server);
 
-const app = express();
-app.use(cors());
+app.get("/", (req, res) => {
+  console.log("get request");
+  res.status(200).send("hello");
+});
 
-io.on("connection", (socket) => {
+io.on("connection", (client) => {
   console.log("a user connected");
+  socket.initializeGame(io, client);
+
+  client.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
 
 app.listen(port, () => {
--- server/socket.js
+++ server/socket.js
@@ -1,21 +1,56 @@
 // need import game initialization
+const gameProcess = require("./gameProcess");
+
 let io;
 let gameSocket;
 let gamesInSession = [];
+let game;
 
 const initializeGame = (sio, socket) => {
   io = sio;
   gameSocket = socket;
   gamesInSession.push(gameSocket);
   gameSocket.on("disconnect", onDisconnect);
-  gameSocket.on("new move", newMove);
+  gameSocket.on("place bomb", pos);
+
+  // gameSocket.on('your turn', )
   gameSocket.on("win", win);
   gameSocket.on("createNewGame", createNewGame);
+  gameSocket.on("playerJoinGame", playerJoinsGame);
 };
 
 function createNewGame(gameId) {
+  game = new gameProcess();
+  game.gameStart();
   this.emit("createNewGame", { gameId: gameId, mySocketId: this.id });
   this.join(gameId);
+}
+
+function playerJoinsGame(idData) {
+  var sock = this;
+  var room = io.sockets.adapter.rooms[idData.gameId];
+
+  if (room === undefined) {
+    this.emit("status", "This game session does not exist.");
+    return;
+  }
+  if (room.length < 2) {
+    idData.mySocketId = sock.id;
+
+    // Join the room
+    sock.join(idData.gameId);
+    console.log(room.length);
+
+    if (room.length === 2) {
+      io.sockets.in(idData.gameId).emit("start game", idData.userName);
+    }
+
+    // Emit an event notifying the clients that the player has joined the room.
+    io.sockets.in(idData.gameId).emit("playerJoinedRoom", idData);
+  } else {
+    // Otherwise, send an error message back to the player.
+    this.emit("status", "There are already 2 people playing in this room.");
+  }
 }
 
 function newMove(move) {
@@ -33,3 +68,5 @@ function onDisconnect() {
 }
 
 exports.initializeGame = initializeGame;
+exports.io = io;
+exports.gameSocket = gameSocket;
